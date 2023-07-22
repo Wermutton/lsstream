9e20cd2816a5d559803fbd2fc615957d6fdcf772
Wermutton <manimateus99@gmail.com>
2023-07-22 09:53:56 -0700

    test

--- src/lsstream/style.py
+++ src/lsstream/style.py
@@ -1,5 +1,9 @@
 from pyfiglet import Figlet
 from termcolor import colored
+from colorama import init
+
+# Initialize colorama
+init()
 
 # For the intro
 intro = Figlet(font='big')
@@ -7,5 +11,3 @@ intro = Figlet(font='big')
 # Just coloring for fun
 def color(text, color='yellow'):
     return colored(text, color, attrs=["bold"])
-
-print("hey")
